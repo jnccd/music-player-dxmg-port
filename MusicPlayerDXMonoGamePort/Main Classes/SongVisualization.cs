11cd416dd605391af71b901b00a51d9f293931c0
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-04-09 21:41:16 +0200

    Fix namespace names

--- MusicPlayerDXMonoGamePort/Main Classes/SongVisualization.cs
+++ MusicPlayerDXMonoGamePort/Main Classes/SongVisualization.cs
@@ -14,7 +14,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MusicPlayerDXMonoGamePort.Main_Classes
+namespace MusicPlayerDXMonoGamePort
 {
     public class SongVisualization
     {
