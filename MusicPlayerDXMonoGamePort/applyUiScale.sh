4c7a8654fa25c58c16f2f73f1a0d010cb9348422
Niklas Carstensen <kobidogao@yahoo.com>
2024-09-08 01:43:31 +0200

    Change ui scale

--- MusicPlayerDXMonoGamePort/applyUiScale.sh
+++ MusicPlayerDXMonoGamePort/applyUiScale.sh
@@ -1,11 +1,12 @@
 ﻿#!/usr/bin/env sh
 
-UISCALE=2.8125
+UISCALE=2.5
 
+# C# Code
 sed -i "11s/.*/        public const float scaleMult = ${UISCALE}f;/" ./HelperClasses/UiScaling.cs
 
+# Font Sizes
 SIZE="$(awk "BEGIN {printf \"%.0f\", 20*$UISCALE}")"
 sed -i "20s/.*/    <Size>$SIZE<\/Size>/" ./Content/Title.spritefont
-
 SIZE="$(awk "BEGIN {printf \"%.0f\", 14*$UISCALE}")"
 sed -i "20s/.*/    <Size>$SIZE<\/Size>/" ./Content/Font.spritefont
