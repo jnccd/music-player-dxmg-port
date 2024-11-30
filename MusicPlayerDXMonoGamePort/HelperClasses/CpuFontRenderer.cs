4925e0b6418f5123bc7e96c12d3aee18fe6717ef
jnccd <kobidogao@outlook.com>
2024-12-01 00:52:24 +0100

    Fix title font scaling

--- MusicPlayerDXMonoGamePort/HelperClasses/CpuFontRenderer.cs
+++ MusicPlayerDXMonoGamePort/HelperClasses/CpuFontRenderer.cs
@@ -9,22 +9,22 @@ namespace MusicPlayerDXMonoGamePort.HelperClasses
 {
     public class CpuFontRenderer(Font font)
     {
-        Font font = new Font(font.FontFamily, font.Size * superResoultion);
         Bitmap tmpTextBitmap = null, textBitmap = null;
 
-        private const int superResoultion = 3;
+        private const int superResoultion = 2;
 
         public void RenderToTexture(Texture2D tex, string textToDraw, int startPosX = 0, int startPosY = 0, SolidBrush color = null)
         {
             color ??= new(Color.White);
 
             tmpTextBitmap?.Dispose();
-            tmpTextBitmap = new Bitmap(tex.Width * superResoultion, tex.Height * superResoultion);
-            Graphics g = Graphics.FromImage(tmpTextBitmap);
+            tmpTextBitmap = new Bitmap(tex.Width * superResoultion, tex.Height * superResoultion, PixelFormat.Format64bppArgb);
+            using Graphics g = Graphics.FromImage(tmpTextBitmap);
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            g.DrawString(textToDraw, font, color, startPosX * superResoultion, startPosY * superResoultion);
+            using Font tmpFont = new(font.FontFamily, font.Size * superResoultion * (96 / g.DpiX));
+            g.DrawString(textToDraw, tmpFont, color, startPosX * superResoultion, startPosY * superResoultion);
             g.Flush();
             textBitmap?.Dispose();
             textBitmap = ResizeImage(tmpTextBitmap, tmpTextBitmap.Width / superResoultion, tmpTextBitmap.Height / superResoultion);
@@ -34,8 +34,8 @@ namespace MusicPlayerDXMonoGamePort.HelperClasses
 
         public SizeF MeasureString(string textToMeasure)
         {
-            Bitmap tmp = new(1, 1);
-            Graphics g = Graphics.FromImage(tmp);
+            using Bitmap tmp = new(1, 1);
+            using Graphics g = Graphics.FromImage(tmp);
             var size = g.MeasureString(textToMeasure, font);
             return new SizeF(size.Width / superResoultion, size.Height / superResoultion);
         }
@@ -47,19 +47,18 @@ namespace MusicPlayerDXMonoGamePort.HelperClasses
 
             destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            using var graphics = Graphics.FromImage(destImage);
+            
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-                using var wrapMode = new ImageAttributes();
-                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-            }
-
+            using var wrapMode = new ImageAttributes();
+            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+            
             return destImage;
         }
 
